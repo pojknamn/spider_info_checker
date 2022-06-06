fe69925899881e1f256a83c041ff71d253ada09d
pojk <aleksejshmel@ydndex.ru>
2022-06-06 17:29:04 +0300

    fix minor bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name="spider-info-checker",
-	version="0.0.8",
+	version="0.0.9",
 	author="pojk",
 	description="Checks for correct spiders info",
 	url="https://github.com/pojknamn/spider_info_checker",
--- spider_info_checker/check_info.py
+++ spider_info_checker/check_info.py
@@ -20,23 +20,26 @@ def main():
         constant_syntax_tree = ast_from_constant(constant)
         update_sprider_names(spider, spider_names)
         check_terms_n_urls(constant_syntax_tree, constant, errors_to_fix)
-        try:
-            spiders_info = find_spider_info(constant_syntax_tree, constant, errors_to_fix)
+        spiders_info = find_spider_info(constant_syntax_tree, constant, errors_to_fix)
+
+        if spiders_info:
             spiders_info_names = set(spiders_info.keys())
             spider_names = set(spider_names)
             spiders_without_info = spider_names.difference(spiders_info_names)
             wrong_names_in_info = spiders_info_names.difference(spider_names)
-            if spiders_without_info:
-                errors_to_fix.append(
-                    ErrMessages.missing_info.format(spiders_without_info=spiders_without_info)
-                )
-            if wrong_names_in_info:
-                errors_to_fix.append(
-                    ErrMessages.wrong_spider_names.format(wrong_names_in_info=wrong_names_in_info)
-                )
-        except NoSpiderInfo as err_message:
+        else:
             spiders_without_info = spider_names
-            errors_to_fix.append(err_message)
+
+        if spiders_without_info:
+            errors_to_fix.append(
+                ErrMessages.missing_info.format(spiders_without_info=spiders_without_info)
+            )
+
+        if wrong_names_in_info:
+            errors_to_fix.append(
+                ErrMessages.wrong_spider_names.format(wrong_names_in_info=wrong_names_in_info)
+            )
+
         if spiders_info and not isinstance(spiders_info, list):
             validate_spiders_info(spiders_info, errors_to_fix, wrong_names_in_info)
 
